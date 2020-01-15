276992a5eaa84e9e3f1c19991473b1ac0db391e8
vivekj1795 <vivekj1795@gmail.com>
2020-01-15 22:18:28 +0530

    view file

---
+++ view.py
@@ -0,0 +1,47 @@
+def welcome_terminal():
+        print("welcome to the library")
+        print("REGISTER AS NEW STUDENT USER [RS]")
+        print("REGITER AS NEW LIBRARIAN USER [RL]")
+        print("Login_student [S]")
+        print("Login_librarian [L]")
+        print("Forgot Password [F]")
+        print("DELETE your account [D] ")
+        print("Enter your choice")
+
+def Registration():
+        print("Register as Student [S]")
+        print("Register as Librarian [L]")
+
+def Update_Account():
+        print("Update your Student account [S]")
+        print("update your librarian account [L]")
+
+def Delete_Account():
+        print("Delete your student Account [S]")
+        print("Delete your Librarian Account [L]")
+
+def Querystring(string):
+        return "Enter " + string
+
+def invalid_statement():
+        print("your username and password is invalid")
+
+
+def Login_Student():
+        print("Update Your Account Credentials [U]")
+        print("DELETE your account [D] ")
+        print("Books list [B]")
+
+
+def login_Librarian():
+        print("Update Your Account Credentials [U]")
+        print("DELETE books [D] ")
+        print("Add books to library [A]")
+ 
+
+def update_student():
+        print("update student Account[S]")
+        print("update librarian Account[L]:")
+        
+
+
